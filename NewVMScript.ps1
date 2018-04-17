3a7bd33d966a7a5d46d961022bf3f7f31f79f6f4
TimekillerTK <erwartungen@protonmail.com>
2018-04-17 08:04:27 +0200

    Again

--- NewVMScript.ps1
+++ NewVMScript.ps1
@@ -2,5 +2,8 @@
     New VM Steps
     1. Config Firewall to be able to respond and send pings
     2. Turn on admin share
-    3. 
+    3. Rename Computer / Join to Domain
+    4. Set region to EU/Europe 
+
+
 #>
