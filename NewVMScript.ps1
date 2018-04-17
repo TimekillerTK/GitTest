550bfa1a58468c6d4b8d32fee4bc563bf041dbe3
TimekillerTK <erwartungen@protonmail.com>
2018-04-17 08:14:56 +0200

    Added step 1, step 2 in progress

--- NewVMScript.ps1
+++ NewVMScript.ps1
@@ -4,6 +4,12 @@
     2. Turn on admin share
     3. Rename Computer / Join to Domain
     4. Set region to EU/Europe 
-
-
 #>
+
+# Step 1
+New-NetFirewallRule -DisplayName "ICMPv4-In-All" -Direction Inbound -Protocol ICMPv4 -Enabled
+New-NetFirewallRule -DisplayName "ICMPv4-Out-All" -Direction Outbound -Protocol ICMPv4 -Enabled
+
+# Step 2
+New-Item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System
+Set-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -
